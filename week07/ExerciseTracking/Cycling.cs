22bf1c650ccff980399e9b2f85ae61725b36c6b7
Derick Shanana <derickshanana50@gmail.com>
2025-06-17 15:18:13 +0200

    week 7 completed and screenshot added

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,17 @@
+// File: Cycling.cs
+public class Cycling : Activity
+{
+    private double _speedKph;
+
+    public Cycling(string date, int minutes, double speedKph)
+        : base(date, minutes)
+    {
+        _speedKph = speedKph;
+    }
+
+    public override double GetSpeed() => _speedKph;
+
+    public override double GetDistance() => (_speedKph * GetLengthMinutes()) / 60;
+
+    public override double GetPace() => 60 / _speedKph;
+}
---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,17 @@
+// File: Swimming.cs
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance() => _laps * 50 / 1000.0;
+
+    public override double GetSpeed() => (GetDistance() / GetLengthMinutes()) * 60;
+
+    public override double GetPace() => GetLengthMinutes() / GetDistance();
+}
